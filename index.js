1f3e1e2bf5e4fa8caea642b11340aa962d2a74fd
rajeev <rajeevrk9901@gmail.com>
2023-04-19 11:57:04 +0530

    stopwatch done

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
 import { createStore } from 'redux'
 
+// action name constant
+const inc = "increment";
+const dec = "decrement";
+const incrementbyAmt = "increamentByPayload";
+
+
 //Store
 const store = createStore(reducer);
 
 const history = []
 
+
+
 function reducer(state={amount:1}, action){
-    if(action.type=="increment"){
+    if(action.type===inc){
 
     //    Wrong method // immutiable // state.amount = state.amount+1 
          return  {amount: state.amount+1}
+    }
+
+    if(action.type===dec){
+
+        //    Wrong method // immutiable // state.amount = state.amount+1 
+             return  {amount: state.amount-1}
+        }
+
+    if (action.type===incrementbyAmt){
+        return {amount:state.amount + action.payload}
     }
     return state;
 }
@@ -18,14 +36,26 @@ function reducer(state={amount:1}, action){
 // console.log(store.getState());
 
 store.subscribe(()=>{
-    history.push(store.getState())
-    console.log(history);
+    // history.push(store.getState())
+    console.log(store.getState());
 })
 
+// Action Creators
+const increamentByPayload = () =>{
+    return {type:incrementbyAmt, payload:4}
+}
+
+const decrement = () => {
+    return {type: dec}
+}
+
+const increment = () => {
+    return {type: inc}
+}
 
 // This is for taking the action in last link we want to dispach when button click
 setInterval(()=>{
-    store.dispatch({type:"increment"})
+    store.dispatch(increment())
 }, 1000)
 
 
